ef08f38c153f63344e85ca9ffe52fb2ea1afceaa
blaksmatic <admin@Blakss-MacBook-Pro.local>
2016-04-24 00:06:22 -0500

    slide start

--- frontend/source_js/app.js
+++ frontend/source_js/app.js
@@ -1,20 +1,22 @@
 var app = angular.module('mainApp', ['ngRoute', 'AppControllers', 'AppServices']);
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-    when('/home', {
-    templateUrl: 'partials/home.html',
-    controller: 'homeController'
-  }).
-  when('/secondview', {
-    templateUrl: 'partials/secondview.html',
-    controller: 'SecondController'
-  }).
-  when('/function', {
-    templateUrl: 'partials/function.html',
-    controller: 'functionController'
-  }).
-  otherwise({
-    redirectTo: '/home'
-  });
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'homeController'
+    }).when('/secondview', {
+        templateUrl: 'partials/secondview.html',
+        controller: 'SecondController'
+    }).when('/function', {
+        templateUrl: 'partials/function.html',
+        controller: 'functionController'
+    }).otherwise({
+        redirectTo: '/home'
+    });
 }]);
+
+app.run(function ($rootScope) {
+    $rootScope.$on('$viewContentLoaded', function () {
+        $(document).foundation();
+    });
+});
--- frontend/source_js/controllers.js
+++ frontend/source_js/controllers.js
@@ -1,44 +1,42 @@
 var mp4Controllers = angular.module('AppControllers', []);
 
-mp4Controllers.controller('homeController', ['$scope', 'CommonData'  , function($scope, CommonData) {
-  $scope.data = "";
-   $scope.displayText = ""
+mp4Controllers.controller('homeController', ['$scope', 'CommonData', function ($scope, CommonData) {
+    $scope.data = "";
+    $scope.displayText = ""
 
-  $scope.setData = function(){
-    CommonData.setData($scope.data);
-    $scope.displayText = "Data set"
+    $scope.setData = function () {
+        CommonData.setData($scope.data);
+        $scope.displayText = "Data set"
 
-  };
-
-}]);
-
-mp4Controllers.controller('SecondController', ['$scope', 'CommonData' , function($scope, CommonData) {
-  $scope.data = "";
-
-  $scope.getData = function(){
-    $scope.data = CommonData.getData();
-
-  };
+    };
 
 }]);
 
+mp4Controllers.controller('SecondController', ['$scope', 'CommonData', function ($scope, CommonData) {
+    $scope.data = "";
 
-mp4Controllers.controller('functionController', ['$scope', '$http', 'Llamas', '$window' , function($scope, $http,  Llamas, $window) {
+    $scope.getData = function () {
+        $scope.data = CommonData.getData();
 
-  Llamas.get().success(function(data){
-    $scope.llamas = data;
-  });
+    };
+
+}]);
+
+
+mp4Controllers.controller('functionController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+    $scope.distance_min = "50";
+    $scope.distance_max = "200";
 
 
 }]);
 
-mp4Controllers.controller('SettingsController', ['$scope' , '$window' , function($scope, $window) {
-  $scope.url = $window.sessionStorage.baseurl;
+mp4Controllers.controller('SettingsController', ['$scope', '$window', function ($scope, $window) {
+    $scope.url = $window.sessionStorage.baseurl;
 
-  $scope.setUrl = function(){
-    $window.sessionStorage.baseurl = $scope.url;
-    $scope.displayText = "URL set";
+    $scope.setUrl = function () {
+        $window.sessionStorage.baseurl = $scope.url;
+        $scope.displayText = "URL set";
 
-  };
+    };
 
 }]);
