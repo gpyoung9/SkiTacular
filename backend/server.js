822c5e962ef74f79f4866b1209fe0aca2312ece8
gpyoung9 <gpyoung9@gmail.com>
2016-05-05 17:26:59 +0000

    access control

--- backend/server.js
+++ backend/server.js
@@ -48,10 +48,15 @@ require('./app/routes.js')(app, passport);
 
 //Allow CORS so that backend and frontend could pe put on different servers
 var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Authorization, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Authorization, Accept");
+    if ('OPTIONS' == req.method) {
+	    res.sendStatus(200);
+    }
+    else {
+    	next();
+    }
 };
 app.use(allowCrossDomain);
 
