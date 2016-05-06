ce5c8175b4da217e8e9d2f8462de9eea099064d2
System Administrator <benchansky@gmail.com>
2016-05-05 21:37:34 -0500

    zip code issue resolved

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,7 +7,7 @@ var UserSchema   = new mongoose.Schema({
     // name : {type: String, required : true},
     email : {type: String, required : true, unique : true},
     password : {type: String, required : true},
-    zipcode : {type : Number},
+    zipcode : {type : String},
     favoriteResorts : [String]
 });
 
